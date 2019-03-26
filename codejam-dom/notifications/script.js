60b07fa55eded896fb0c3c40dedfe3d790c688b1
Fedor Markovich <Fedor@air-fedor.bsu>
2019-03-26 15:53:07 +0300

    add class Notifications in script.js

---
+++ codejam-dom/notifications/script.js
@@ -0,0 +1,11 @@
+class Notifications {
+    constructor() {
+        this.index = null;
+        this.closeButton = null;
+        this.switches = null;
+        this.text = null;
+        this.input = null;
+        this.notificationsText = ["1","2","3","4","5","6"]
+        this.start();
+    }
+}
